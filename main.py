78d3e86be7527e84a3c27c19590fe8a7755dbcab
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2022-09-07 13:35:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from menu import MENU, profit, resources
+
+# TODO: 1. Ask user what drink they want.
+print("Welcome to Jared's Coffee Machine!")
+print("Our menu is: espresso, latte, and cappuccino.")
+order = input("What would you like from our menu?")
+
+# TODO: 2. Turn coffee machine off to end program.
+machine_off = False
+while not machine_off:
+    
+
+# TODO: 3. Print a report of all coffee machine resources.
+
+
+# TODO: 4. Check resources sufficient to make drink order.
+
+
+# TODO: 5. Process coins
+
+
+# TODO: 6. Check if transaction was successful. If user put enough money in and give them change back.
+
+
+# TODO: 7. Make coffee and deduct resources used.
+
